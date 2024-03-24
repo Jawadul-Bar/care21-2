6f89eeb708ee1fc7e22838986a494e26be7d0815
Jawadul <jawadulbar2711@gmail.com>
2024-03-24 20:01:06 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,16 @@ module.exports = {
     },
     plugins: [],
   }
+
+  // custom screens
+module.exports = {
+  theme: {
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+  },
+}
